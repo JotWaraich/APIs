1c30949e7bc6a094a0b36ae62ea3663078a8b3d6
Jot Waraich <gurjagjot@gmail.com>
2024-06-01 15:43:20 -0600

    testing

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const port = 2001;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const QRCode = require("qrcode");
 app.get("/api/qrcode", async (req, res) => {
   try {
     const url = req.query.url || "https://example.com";
     const qrCodeImage = await QRCode.toDataURL(url);
-    res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
+    res.send(
+      `<img src="${qrCodeImage}" alt="QR Code" style="height: 200px; width: 200px"/>`
+    );
   } catch (err) {
     console.error("Error generating QR code:", err);
     res.status(500).send("Internal Server Error");
